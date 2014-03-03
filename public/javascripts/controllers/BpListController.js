08325446234346ada009ed8e963f80a6a02bed77
wastegas <wastegas@gmail.com>
2014-03-02 20:28:50 -1000

    clean up source formatting

--- public/javascripts/controllers/BpListController.js
+++ public/javascripts/controllers/BpListController.js
@@ -1,4 +1,5 @@
-function BPListController($scope, $http) {
+function BPListController ($scope, $http) {
+    'use strict';
     $scope.pressures = [];
 
     $scope.newPressure = {
@@ -8,20 +9,20 @@ function BPListController($scope, $http) {
         diastolic: null,
         pulse: null
     };
-    
-    $scope.setPressures = function(pressures) {
+
+    $scope.setPressures = function (pressures) {
         $scope.pressures = pressures;
     };
 
-    $scope.addNewPressure = function() {
-        $http.post('/pressure.json', $scope.newPressure).success(function(data) {
-            if(data.pressure) {
+    $scope.addNewPressure = function () {
+        $http.post('/pressure.json', $scope.newPressure).success(function (data) {
+            if (data.pressure) {
                 $scope.pressures.push(data.pressure);
                 $scope.newPressure.systolic = null;
                 $scope.newPressure.diastolic = null;
                 $scope.newPressure.pulse = null;
             } else {
-                alert(JSON.stringify(data));
+                alert (JSON.stringify(data));
             }
         });
     };
@@ -31,26 +32,25 @@ function BPListController($scope, $http) {
         { name: 'Evening', value: 'Evening' }
     ];
 
-    $scope.pressure = { timeofday : 
-        $scope.timeOptions[0].value};
+    $scope.pressure = {
+        timeofday : $scope.timeOptions[0].value
+    };
 
-    $scope.selection = function() {
+    $scope.selection = function () {
         console.log($scope.pressure);
     };
 
     $scope.getClass = function(pressure) {
-        if(pressure.systolic <= 120 && pressure.diastolic < 80) {
+        if (pressure.systolic <= 120 && pressure.diastolic < 80) {
             return 'success';
-        } else if(pressure.systolic >= 140 && pressure.systolic <=159 || pressure.diastolic >= 90 && pressure.diastolic <= 99) {
-                return 'stage1';
-        } else if(pressure.systolic > 120 && pressure.systolic <= 139 || pressure.diastolic >= 80 && pressure.diastolic <= 89) {
-                return 'warning';
+        } else if (pressure.systolic >= 140 && pressure.systolic <=159 || pressure.diastolic >= 90 && pressure.diastolic <= 99) {
+            return 'stage1';
+        } else if (pressure.systolic > 120 && pressure.systolic <= 139 || pressure.diastolic >= 80 && pressure.diastolic <= 89) {
+            return 'warning';
         } else {
             return 'danger';
         }
 
         console.log(pressure.systolic);
     };
-
-
 }
